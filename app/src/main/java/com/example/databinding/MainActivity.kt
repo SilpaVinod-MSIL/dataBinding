dcbab912ce98f01879e3208aeafee09e93960cb3
silpa.vinod <silpa.vinod@marketsimplified.com>
2022-05-06 17:47:57 +0530

    Data binding in Kotlin

---
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.btnSubmit.setOnClickListener {
+            displayName()
+        }
+    }
+
+    private fun displayName() {
+        binding.mainTextView.text=binding.getNameEt.text
+    }
+}
